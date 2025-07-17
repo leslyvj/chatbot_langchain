6bfc7e5f73fa4a141ec613c96feaf055a38541c0
Lesly V J <leslychittattukara@gmail.com>
2025-07-17 10:10:18 +0530

    Update main.py

--- chatbot/main.py
+++ chatbot/main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 #from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.output_parsers import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser
 from langchain_community.llms import Ollama
 
 
